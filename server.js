87c7c8986f2a39b0f8d7679c2dbac3b753d4fef4
Matt Szabo <wekilledit@users.noreply.github.com>
2016-09-26 14:16:19 +1000

    added user schema

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var User = require('./app/models/user');
 
+var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017');
 
 var port = process.env.PORT || 8080;
 
