d46a57b6d1b76d3a7072fcbdfededfefbb6fe565
Matt Szabo <wekilledit@users.noreply.github.com>
2016-09-26 13:48:13 +1000

    fixed typo in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencode({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
