0651b709c6b8c71046df67f89c02790b5ea34f39
Matt Szabo <wekilledit@users.noreply.github.com>
2016-09-26 14:33:36 +1000

    added post and get for /users to the API

--- server.js
+++ server.js
@@ -13,9 +13,35 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+  console.log('Something\'s happening... ' + req.body);
+  next();
+});
+
 router.get('/', function(req, res) {
   res.json({ message: 'Welcome to Matt\'s node API'});
 });
+
+router.route('/users')
+  .post(function(req, res) {
+    var user = new User();
+    user.name = req.body.name;
+
+    user.save(function(err) {
+      if(err) {
+        res.send(err);
+      }
+      res.json({ message: 'User created!' })
+    })
+  })
+  .get(function(req, res) {
+    User.find(function(err, users) {
+      if(err) {
+        res.send(err);
+      }
+      res.json(users);
+    });
+  })
 
 // all of our routes will be prefixed by /api
 app.use('/api', router);
